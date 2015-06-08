b66989a8c2f228b20914c9a9fcbf0e9f49fee309
Eugene Skinner <eskinfrn@gmail.com>
2015-06-08 08:47:05 -0600

    Has been linted and tested. Everything passing.

--- helloworld.js
+++ helloworld.js
@@ -1,28 +1,28 @@
 var basic = {
-  sayHello() {
-    return 'hello world';
+  sayHello () {
+    return 'hello world'
   },
 
-  doMath(a = 2, b = 2) {
-    return a + b;
+  doMath (a = 2, b = 2) {
+    return a + b
   },
 
-  randomArray:  [1,2,3,4,5],
+  randomArray: [1, 2, 3, 4, 5],
 
-  doesExist(array,x) {
-    return array.includes(x);
+  doesExist (array, x) {
+    return array.includes(x)
   },
 
-  findGreatest(array) {
-    var storage = 0;
-    for(let i of array){
-      if(i > storage){
-        storage = i;
+  findGreatest (array) {
+    var storage = 0
+    for (let i of array) {
+      if (i > storage) {
+        storage = i
       }
     }
-    return storage;
+    return storage
   }
 
-};
+}
 
-module.exports = basic;
+module.exports = basic
